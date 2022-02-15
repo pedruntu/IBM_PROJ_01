0c0f41dba799d9e92879723bd41bfb25b684bf85
pedruntu <max.iskram@gmail.com>
2022-02-15 16:12:49 -0300

    working ok on ibmcloud

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -100,11 +100,11 @@ class Enrollment(models.Model):
     # Has a One-To-Many (or Many-To-Many if you want to reuse questions) relationship with course
     # Has a grade point for each question
     # Has question content
-    # Other fields and methods you would like to design
-#class Question(models.Model):
-    # Foreign key to lesson
-    # question text
-    # question grade/mark
+    # Other fields and methods you would like to design   
+class Question(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)  # Foreign key to course (not to lesson)
+    question_text = models.CharField(max_length=200, default="question") # question text
+    grade = models.IntegerField(default=0) # question grade/mark
 
     # <HINT> A sample model method to calculate if learner get the score of the question
     #def is_get_score(self, selected_ids):
@@ -122,13 +122,18 @@ class Enrollment(models.Model):
     # Choice content
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
-# class Choice(models.Model):
+class Choice(models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE) # One-To-Many relationship with Question
+    choice_text = models.CharField(max_length=200, default="choice") # Choice text
+    is_correct = models.BooleanField(default=False) # True si es la opción correcta
+
+
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
