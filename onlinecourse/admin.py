4a75bce032a0da0541a09b683d09143733fb0050
pedruntu <max.iskram@gmail.com>
2022-02-22 17:14:37 -0300

    seems to be finished

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -2,31 +2,32 @@ from django.contrib import admin
 # <HINT> Import any new Models here
 from .models import Course, Enrollment, Lesson, Instructor, Learner
 from .models import Question, Choice,Submission, Answer
+
 # <HINT> Register QuestionInline and ChoiceInline classes here
 class QuestionInline(admin.StackedInline):
     model = Question
-    #extra = 5
+    extra = 0
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
-    #extra = 5
+    extra = 0
 
 
 class LessonInline(admin.StackedInline):
     model = Lesson
-    extra = 3
+    extra = 0
 
 class EnrollmentInline(admin.StackedInline):
     model = Enrollment
-    #extra = 4
+    extra = 0
 
 class SubmissionInline(admin.StackedInline):
     model = Submission
-    #extra = 5
+    extra = 0
 
 class AnswerInline(admin.StackedInline):
     model = Answer
-    #extra = 4
+    extra = 0
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
@@ -34,6 +35,7 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
+    
 
 class EnrollmentAdmin(admin.ModelAdmin):
     inlines = [SubmissionInline]
@@ -41,17 +43,19 @@ class EnrollmentAdmin(admin.ModelAdmin):
 
 
 class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
+    list_display = ['title','course_id']
 
 # <HINT> Register Question and Choice models here
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ('question_text', 'grade')
-    #list_filter = ['pub_date']
+    list_filter = ['course']
     #search_fields = ['name', 'description']
 
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = ('choice_text', 'is_correct')
+    list_filter = ['question_id']
+
 
 class SubmissionAdmin(admin.ModelAdmin):
     inlines = [AnswerInline]
@@ -82,4 +86,3 @@ admin.site.register(Instructor)
 admin.site.register(Learner)
 admin.site.register(Enrollment, EnrollmentAdmin)
 admin.site.register(Submission, SubmissionAdmin)
-#admin.site.register(Answer, AnswerAdmin)
